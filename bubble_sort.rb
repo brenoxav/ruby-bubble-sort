a331b8703d123757384b209a056e793b254b110f
cgcarlosg <cgcarlosg@hotmail.com>
2020-11-13 15:32:13 -0500

    second part of the project

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -20,3 +20,24 @@ array = [1,2,3,4,5,9,8,7,6]
 p bubble_sort(array)
 
 # bubble_sort_by
+
+def bubble_sort_by(arr)
+  iterations = 0
+  loop do
+      arr_changed = false
+      (arr.length-1).times do |index|
+          if yield(arr[index], arr[index+1]) > 0
+              arr[index], arr[index+1] = arr[index+1], arr[index]
+              arr_changed = true
+          end
+      end
+      iterations += 1
+      break if arr_changed == false
+  end
+  p arr
+  arr
+end
+
+bubble_sort_by(["hey","hello","hi"]) do |left, right|
+  left.length - right.length
+end
