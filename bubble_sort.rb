63abee8f5e92655b6b81c0a5afda6ae0e86bbb6b
Breno Xavier <brenoxav@gmail.com>
2020-11-13 17:46:22 -0400

    Fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,43 +1,44 @@
+# frozen_string_literal: true
+
 # bubble_sort
 def bubble_sort(array)
   iterations = 0
-  
-   loop do
-      array_changed = false
-      (array.length-1).times do |index|
-        if (array[index] <=> array[index+1]) == 1
-          array[index], array[index+1] = array[index+1], array[index]
-          array_changed = true 
-        end
+  loop do
+    array_changed = false
+    (array.length - 1).times do |index|
+      if (array[index] <=> array[index + 1]) == 1
+        array[index], array[index + 1] = array[index + 1], array[index]
+        array_changed = true
       end
-      iterations += 1
-      break if !array_changed
     end
+    iterations += 1
+    break unless array_changed
+  end
   array
 end
 
-array_1 = [1,2,3,4,5,9,8,7,6]
-p bubble_sort(array_1)
+arr1 = [1, 2, 3, 4, 5, 9, 8, 7, 6]
+p bubble_sort(arr1)
 
 # bubble_sort_by
 def bubble_sort_by(arr)
   iterations = 0
   loop do
-      arr_changed = false
-      (arr.length-1).times do |index|
-          if yield(arr[index], arr[index+1]) > 0
-              arr[index], arr[index+1] = arr[index+1], arr[index]
-              arr_changed = true
-          end
+    arr_changed = false
+    (arr.length - 1).times do |index|
+      if yield(arr[index], arr[index + 1]).positive?
+        arr[index], arr[index + 1] = arr[index + 1], arr[index]
+        arr_changed = true
       end
-      iterations += 1
-      break if arr_changed == false
+    end
+    iterations += 1
+    break if arr_changed == false
   end
   arr
 end
 
-array_2 = bubble_sort_by(["hey","hello","hi"]) do |left, right|
+arr2 = bubble_sort_by(%w[hey hello hi]) do |left, right|
   left.length - right.length
 end
 
-p array_2
+p arr2
